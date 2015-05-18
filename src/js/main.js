35c78c7304673341406841e36226f7863b4017ec
yaskyj <yaskyj@yahoo.com>
2015-05-18 13:03:01 -0500

    Empty screen added along with all assets

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,30 @@
 window.onload = function () {
   'use strict';
 
-  var game
-    , ns = window['super-crate-box'];
+  var game;
+    //, ns = window['super-crate-box'];
+  var mainState = {
+    preload: function() {
 
-  game = new Phaser.Game(640, 480, Phaser.AUTO, 'super-crate-box-game');
-  game.state.add('boot', ns.Boot);
-  game.state.add('preloader', ns.Preloader);
-  game.state.add('menu', ns.Menu);
-  game.state.add('game', ns.Game);
+    },
+
+    create: function() {
+      game.stage.backgroundColor = '#3498db';
+      game.physics.startSystem(Phaser.Physics.ARCADE);
+    },
+
+    update: function() {
+
+    }
+  };
+  game = new Phaser.Game(500, 340, Phaser.AUTO, 'super-crate-box-game');
+  game.state.add('main', mainState);
+  game.state.start('main');
+  //game.state.add('boot', ns.Boot);
+  //game.state.add('preloader', ns.Preloader);
+  //game.state.add('menu', ns.Menu);
+  //game.state.add('game', ns.Game);
   /* yo phaser:state new-state-files-put-here */
 
-  game.state.start('boot');
+  //game.state.start('boot');
 };
