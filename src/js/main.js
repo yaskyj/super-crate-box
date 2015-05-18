97cd52ce565311e0b6288778e3d7e55449a09af7
yaskyj <yaskyj@yahoo.com>
2015-05-18 13:56:53 -0500

    Walls, collisions, and player respawn added

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,8 @@ window.onload = function () {
   var mainState = {
     preload: function() {
       game.load.image('player', 'assets/player.png');
+      game.load.image('wallV', 'assets/wallVertical.png');
+      game.load.image('wallH', 'assets/wallHorizontal.png');
     },
 
     create: function() {
@@ -16,10 +18,15 @@ window.onload = function () {
       game.physics.arcade.enable(this.player);
       this.player.body.gravity.y = 500;
       this.cursor = game.input.keyboard.createCursorKeys();
+      this.createWorld();
     },
 
     update: function() {
+      game.physics.arcade.collide(this.player, this.walls);
       this.movePlayer();
+      if (!this.player.inWorld) {
+        this.playerDie();
+      }
     },
 
     movePlayer: function() {
@@ -36,8 +43,32 @@ window.onload = function () {
       if (this.cursor.up.isDown && this.player.body.touching.down) {
         this.player.body.velocity.y = -320;
       }
+    },
+
+    createWorld: function() {
+      // Create our wall group with Arcade physics
+      this.walls = game.add.group();
+      this.walls.enableBody = true;
+      // Create the 10 walls
+      game.add.sprite(0, 0, 'wallV', 0, this.walls); // Left
+      game.add.sprite(480, 0, 'wallV', 0, this.walls); // Right
+      game.add.sprite(0, 0, 'wallH', 0, this.walls); // Top left
+      game.add.sprite(300, 0, 'wallH', 0, this.walls); // Top right
+      game.add.sprite(0, 320, 'wallH', 0, this.walls); // Bottom left
+      game.add.sprite(300, 320, 'wallH', 0, this.walls); // Bottom right
+      game.add.sprite(-100, 160, 'wallH', 0, this.walls); // Middle left
+      game.add.sprite(400, 160, 'wallH', 0, this.walls); // Middle right
+      var middleTop = game.add.sprite(100, 80, 'wallH', 0, this.walls); middleTop.scale.setTo(1.5, 1);
+      var middleBottom = game.add.sprite(100, 240, 'wallH', 0, this.walls); middleBottom.scale.setTo(1.5, 1);
+      // Set all the walls to be immovable
+      this.walls.setAll('body.immovable', true);
+    },
+
+    playerDie: function () {
+      game.state.start('main');
     }
   };
+
   game = new Phaser.Game(500, 340, Phaser.AUTO, 'super-crate-box-game');
   game.state.add('main', mainState);
   game.state.start('main');
